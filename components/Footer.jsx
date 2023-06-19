aabc92e6bd625e980b5bed79b58aefdceef2acaf
Atulaya123 <115982827+Atulaya123@users.noreply.github.com>
2023-06-19 12:46:51 +0530

    Update Footer.jsx
    Added links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+import { AiFillInstagram, AiOutlineTwitter, AiFillGithub } from "react-icons/ai";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 ClaireWei All rights reserverd</p>
+      <p>2023 Atulya Vaibhav Rai All rights reserved</p>
       <p className="icons">
+        <Link href="https://www.instagram.com/raiatulya">
+          <a>
         <AiFillInstagram />
+        </a>
+        </Link>
+        <Link href="https://twitter.com/AtulyaVaibhav">
+          <a>
         <AiOutlineTwitter />
+        </a>
+        </Link>
+        <Link href="https://github.com/Atulaya123">
+          <a>
+            <AiFillGithub />
+          </a>
+        </Link>
       </p>
     </div>
   );
